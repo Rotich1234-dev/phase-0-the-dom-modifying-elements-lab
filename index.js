c36f5caad4d1484b159c8b8f13f4f4413e585bed
Rotich <jane.rotich@student.moringaschool.com>
2024-01-05 20:51:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+//  To remove the <main> with id 'main'
+const mainElement = document.querySelector('main#main');
+if (mainElement) {
+  mainElement.remove();
+}
+
+//  Create a new <h1> with id 'victory'
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+//  the newHeader variable should point to node 'h1#victory'
+const newHeaderElement = document.querySelector('h1#victory');
+
+// YOUR-NAME is the champion
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+
